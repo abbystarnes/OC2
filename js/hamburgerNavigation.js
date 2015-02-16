67eb5ea7e2c0030f7c6540aa0ef21f9c66f44b69
Abby Starnes <ordette.starnes@gmail.com>
2015-02-16 01:40:28 -0500

    new layout

---
+++ js/hamburgerNavigation.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+	$(".hidingNav1").hide();
+
+	$("#myLink").click(function(){
+		$(".hidingNav1").slideToggle('slow');
+	});
+
+});
+
+$(document).ready(function(){
+	$(".hidingNav2").hide();
+
+	$("#myLink2").click(function(){
+		$(".hidingNav2").slideToggle('slow');
+	});
+
+});
+
+$(document).ready(function(){
+	$(".hidingNav3").hide();
+
+	$("#myLink3").click(function(){
+		$(".hidingNav3").slideToggle('slow');
+	});
+
+});
+
+$(document).ready(function(){
+	$(".hidingNav4").hide();
+
+	$("#myLink4").click(function(){
+		$(".hidingNav4").slideToggle('slow');
+	});
+
+});
+
+
+$(document).ready(function(){
+	$(".hidingNav8").hide();
+
+	$("#myLink8").click(function(){
+		$(".hidingNav8").slideToggle('slow');
+	});
+
+});
+
+
+
+
