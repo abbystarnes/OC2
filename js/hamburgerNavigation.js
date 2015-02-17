fd35f98a987e40848cd4a2e538afeb6ff68b4a84
Abby Starnes <ordette.starnes@gmail.com>
2015-02-16 23:45:08 -0500

    broken but more

--- js/hamburgerNavigation.js
+++ js/hamburgerNavigation.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 
 	//$(".menuTrigger h2 a").click(function(){
 	//	$(this).parent().parent().children(".dropdown").toggleClass('open');
@@ -7,6 +7,16 @@ $(document).ready(function(){
 
 	$("#myLink8").click(function(){
 		$(".hidingNav8").toggleClass('open');
+	});
+
+});*/
+
+
+/*$(document).ready(function(){
+	$(".hidingNav").hide();
+
+	$("#myLink8").click(function(){
+		$(".hidingNav").slideToggle('slow');
 	});
 
 });
