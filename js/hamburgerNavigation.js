258c0de77a234ad4bb5311e203e0254dcc8c32e8
Abby Starnes <ordette.starnes@gmail.com>
2015-02-18 13:40:27 -0500

    dot

--- js/hamburgerNavigation.js
+++ js/hamburgerNavigation.js
@@ -1,21 +1,26 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
 
-	//$(".menuTrigger h2 a").click(function(){
-	//	$(this).parent().parent().children(".dropdown").toggleClass('open');
-	//});
-
-
-	$("#myLink8").click(function(){
-		$(".hidingNav8").toggleClass('open');
+	$(".menuTrigger h2 a").click(function(){
+		$(this).parent().parent().children(".dropdown").toggleClass('open');
 	});
 
-});*/
+
+	// $("#myLink8").click(function(){
+		// $(".hidingNav8").toggleClass('open');
+	// });
+
+});
 
 
-/*$(document).ready(function(){
+
+
+
+ 
+
+$(document).ready(function(){
 	$(".hidingNav").hide();
 
-	$("#myLink8").click(function(){
+	$("#myLink").click(function(){
 		$(".hidingNav").slideToggle('slow');
 	});
 
